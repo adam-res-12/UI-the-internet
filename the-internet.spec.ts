1be79657449aa323a95cd34a8ccfbdc724aa7d29
adam-res-12 <adam.chadwick@resillion.com>
2025-10-06 13:42:12 +0100

    test to look at title

---
+++ the-internet.spec.ts
@@ -0,0 +1,26 @@
+import {test, expect} from '@playwright/test';
+
+test.beforeEach(async({page}) => {
+    await page.goto("https://the-internet.herokuapp.com/");
+});
+
+// Screenshot for failed tests:
+test.afterEach(async({page}, testInfo) => {
+    if (testInfo.status == "failed") {
+        const screenshot = await page.screenshot({
+            path: `screenshots/${testInfo.title}.png`,
+            fullPage: true,
+        });
+        await testInfo.attach("screenshot", {
+            body: screenshot,
+            contentType: "image/png",
+        });
+    }
+});
+
+test('has title', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+
+    await expect(page).toHaveTitle(/The Internet/);
+});
+
