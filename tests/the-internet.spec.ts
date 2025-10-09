009e5a98a20650f7611dc2219592c1323973df83
adam-res-12 <adam.chadwick@resillion.com>
2025-10-09 09:40:59 +0100

    alphabetical order/how it appears on the-internet

--- tests/the-internet.spec.ts
+++ tests/the-internet.spec.ts
@@ -28,6 +28,46 @@ test('has heading', async ({page}) => {
     await expect(page.getByRole('heading', {name: 'Welcome to the-internet'})).toBeVisible();
 });
 
+//Add/Remove Elements - add delete, then check its there
+// add 2 delete, then check for 2
+// add delete then remove, check delete is removed
+test('addElement', async ({page}) => {
+    await page.getByRole('link', {name: 'Add/Remove Elements'}).click();
+
+    await page.getByRole('button', {name: 'Add Element'}).click();
+
+    await expect(page.getByRole('button', {name: 'Delete'})).toBeVisible();
+});
+
+test('add2Elements', async ({page}) => {
+    await page.getByRole('link', {name: 'Add/Remove Elements'}).click();
+
+    await page.getByRole('button', {name: 'Add Element'}).click({clickCount: 2});
+
+    await expect(page.getByRole('button', {name: 'Delete'})).toHaveCount(2);
+});
+
+test('deleteElement', async ({page}) => {
+    await page.getByRole('link', {name: 'Add/Remove Elements'}).click();
+
+    await page.getByRole('button', {name: 'Add Element'}).click();
+
+    await page.getByRole('button', {name: 'Delete'}).click();
+
+    await expect(page.getByRole('button', {name: 'Delete'})).toHaveCount(0);
+});
+
+//Basic Auth - puts username and password into url to sign in, then checks heading that only appears after sign in
+test('signIn', async ({page}) => {
+    const username = 'admin';
+
+    const password = 'admin';
+
+    await page.goto(`https://${username}:${password}@the-internet.herokuapp.com/basic_auth`);
+
+    await expect(page.getByRole('heading', {name: 'Basic Auth'})).toBeVisible();
+});
+
 //Checkboxes - check box 1, then check if checked
 // uncheck box 2, then check if not checked
 test('checkbox1', async ({page}) => {
@@ -73,62 +113,6 @@ test('dropdownOption2', async ({page}) => {
     await expect(page.locator('#dropdown')).toHaveValue('2');
 });
 
-//Redirection - checks that the redirect link takes you to the status codes page
-test('redirection', async ({page}) => {
-    await page.getByRole('link', {name: 'Redirect Link'}).click();
-
-    await page.locator('#redirect').click();
-
-    await expect(page.getByRole('heading', {name:'Status Codes'})).toBeVisible();
-});
-
-//Key Presses - checks if pressing shift button in box results in 'You entered: SHIFT' output
-// then same for Q
-test('keyPressesSHIFT', async ({page}) => {
-    await page.getByRole('link', {name: 'Key Presses'}).click();
-
-    await page.locator('#target').press('Shift');
-
-    await expect(page.locator('#result')).toHaveText('You entered: SHIFT');
-});
-
-test('keyPressesQ', async ({page}) => {
-    await page.getByRole('link', {name: 'Key Presses'}).click();
-
-    await page.locator('#target').press('Q');
-
-    await expect(page.locator('#result')).toHaveText('You entered: Q');
-});
-
-//Add/Remove Elements - add delete, then check its there
-// add 2 delete, then check for 2
-// add delete then remove, check delete is removed
-test('addElement', async ({page}) => {
-    await page.getByRole('link', {name: 'Add/Remove Elements'}).click();
-
-    await page.getByRole('button', {name: 'Add Element'}).click();
-
-    await expect(page.getByRole('button', {name: 'Delete'})).toBeVisible();
-});
-
-test('add2Elements', async ({page}) => {
-    await page.getByRole('link', {name: 'Add/Remove Elements'}).click();
-
-    await page.getByRole('button', {name: 'Add Element'}).click({clickCount: 2});
-
-    await expect(page.getByRole('button', {name: 'Delete'})).toHaveCount(2);
-});
-
-test('deleteElement', async ({page}) => {
-    await page.getByRole('link', {name: 'Add/Remove Elements'}).click();
-
-    await page.getByRole('button', {name: 'Add Element'}).click();
-
-    await page.getByRole('button', {name: 'Delete'}).click();
-
-    await expect(page.getByRole('button', {name: 'Delete'})).toHaveCount(0);
-});
-
 //Entry Ad - check ad/modal window appears when clicking on link
 // close ad/modal window, then check it's no longer there
 // close ad/modal window and re-enable, then check that ad/modal window is showing again
@@ -156,13 +140,29 @@ test('re-enableAd', async ({page}) => {
     await expect(page.getByRole('heading', {name: 'This is a modal window'})).toBeVisible();
 });
 
-//Basic Auth - doesn't work - don't know how to fill in box at the moment
-test('signIn', async ({page}) => {
-    const username = 'admin';
+//Key Presses - checks if pressing shift button in box results in 'You entered: SHIFT' output
+// then same for Q
+test('keyPressesSHIFT', async ({page}) => {
+    await page.getByRole('link', {name: 'Key Presses'}).click();
 
-    const password = 'admin';
+    await page.locator('#target').press('Shift');
 
-    await page.goto(`https://${username}:${password}@the-internet.herokuapp.com/basic_auth`);
+    await expect(page.locator('#result')).toHaveText('You entered: SHIFT');
+});
 
-    await expect(page.getByRole('heading', {name: 'Basic Auth'})).toBeVisible();
+test('keyPressesQ', async ({page}) => {
+    await page.getByRole('link', {name: 'Key Presses'}).click();
+
+    await page.locator('#target').press('Q');
+
+    await expect(page.locator('#result')).toHaveText('You entered: Q');
+});
+
+//Redirection - checks that the redirect link takes you to the status codes page
+test('redirection', async ({page}) => {
+    await page.getByRole('link', {name: 'Redirect Link'}).click();
+
+    await page.locator('#redirect').click();
+
+    await expect(page.getByRole('heading', {name:'Status Codes'})).toBeVisible();
 });
